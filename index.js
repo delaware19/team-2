9e5a53b163b3b83469deea1198b0928a778067a7
unknown <shreyas.j.patil@gmail.com>
2019-09-28 01:21:41 -0400

    Does not work Backend

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ var sql = require("mssql");
 var config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    server: 'localhost', 
-    database: process.env.DATABASE
+    server: '0.0.0.0', 
+    database: process.env.DATABASE,
+    port: 1434
 };
+
+console.log(config);
+
 
 const app = express();
 
@@ -20,8 +24,24 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 
 app.get("/", (req,res) =>{
+
     sql.connect(config, (err)=>{
-       
+
+
+        if(err) console.log(err);
+        
+        var request = new sql.Request();
+           
+       //query to the database and get the records
+        request.query('Select * from Delaware$;', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+       // res.send("Error?");
     });
 });
 
